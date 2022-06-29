0f4d03c2260c1de64414ee717f43396a3985ca03
xiaobincen <xiaobincen@tencent.com>
2022-06-29 16:16:19 +0800

    chore: release test

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,10 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'docs',
+      assets: 'docs'
+    }),
     paths: {
       base: isDev ? '' : '/static-poke-dex'
     }
