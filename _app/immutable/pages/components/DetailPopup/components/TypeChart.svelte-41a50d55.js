fd827f0daeb1414c269e19eb72d3acb7c6bfa9f2
pineapplebin <pineapplewing@126.com>
2022-07-26 21:17:25 +0800

    Updates

---
+++ _app/immutable/pages/components/DetailPopup/components/TypeChart.svelte-41a50d55.js
@@ -0,0 +1,1 @@
+import{S as i,i as o,s as r}from"../../../../chunks/index-feed1324.js";function l(e,t,a){let{types:s}=t;return e.$$set=n=>{"types"in n&&a(0,s=n.types)},[s]}class u extends i{constructor(t){super(),o(this,t,l,null,r,{types:0})}}export{u as default};
